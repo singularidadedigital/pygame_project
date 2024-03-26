bfd79923c12f46af98644a0094f0b616c32ab0ce
Rodrigo Luciano da Costa <rodrigolucianocosta@gmail.com>
2024-03-26 13:47:21 -0300

    change largura/altura to WIDTH/HEIGHT

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame
 from pygame import display, transform
 from pygame.image import load
@@ -6,12 +8,12 @@ from pygame.locals import *
 # pygame setup
 pygame.init()
 
-largura = 480
-altura = 320
+WIDTH = 500
+HEIGHT = 320
 
-screen = display.set_mode(size=(largura, altura))
+screen = display.set_mode(size=(WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-
+font = pygame.font.SysFont(None, 40)
 dt = 0
 
 # carregar imagem de fundo
@@ -20,22 +22,99 @@ background = load('images/nature_5/orig.png')
 # Music
 pygame.mixer.music.load("assets/EssentialGameAudiopack/FullScores/Orchestral Scores/Ove_Melaa-Heaven_Sings.mp3")
 pygame.mixer.music.play(-1)
-
 running = True
-if __name__ == "__main__":
-    while running:
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topright = (x, y)
+    surface.blit(textobj, textrect)
+
+
+click = False
+
+
+def main_menu():
+    while True:
+
+        # text menu
+        draw_text('Nova Genesis', font, (255, 255, 255), screen, 455, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        # Button 1
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+
+        click = False
+
         # poll for events
         # pygame QUIT event means the user clicked X to close windows
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
 
         # RENDER THE GAME
         # Espaco do display
         screen.blit(background, (0, 0))
-        display.flip()
+
+        # text menu
+        draw_text('Nova Genesis', font, (255, 255, 255), screen, 200, 20)
+        pygame.display.update()
 
         # LIMIT FPS
-        dt = clock.tick(60) / 1000
+        clock.tick(60) / 1000
 
-pygame.quit()
+
+def game():
+    running = True
+    while running:
+        draw_text('game start', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        pygame.display.update()
+        clock.tick(60) / 1000
+
+
+def options():
+    running = True
+    while running:
+        draw_text('game start', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        pygame.display.update()
+        clock.tick(60) / 1000
+
+
+# if "__name__" == "__main__":
+main_menu()
